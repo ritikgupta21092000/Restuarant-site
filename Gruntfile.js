6c7a1b63e8be2fac54ca22ee4b38305b680d0b0c
Ritik Gupta <guptaritik349@gmail.com>
2020-07-02 13:58:06 +0530

    updated Grunt configuration

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,7 +8,9 @@ module.exports = function (grunt) {
     require('time-grunt')(grunt);
 
     // Automatically load required Grunt tasks
-    require('jit-grunt')(grunt);
+    require('jit-grunt')(grunt, {
+        useminPrepare: "grunt-usemin"
+    });
 
     // Define the configuration for all the tasks
     grunt.initConfig({
@@ -78,6 +80,80 @@ module.exports = function (grunt) {
                     dest: 'dist/'
                 }]
             }
+        },
+        useminPrepare: {
+            foo: {
+                dest: 'dist',
+                src: ['contactus.html','aboutus.html','index.html']
+            },
+            options: {
+                flow: {
+                    steps: {
+                        css: ['cssmin'],
+                        js:['uglify']
+                    },
+                    post: {
+                        css: [{
+                            name: 'cssmin',
+                            createConfig: function (context, block) {
+                            var generated = context.options.generated;
+                                generated.options = {
+                                    keepSpecialComments: 0, rebase: false
+                                };
+                            }       
+                        }]
+                    }
+                }
+            }
+        },
+        concat: {
+            options: {
+                separator: ';'
+            },
+            // dist configuration is provided by useminPrepare
+            dist: {}
+        },
+
+        // Uglify
+        uglify: {
+            // dist configuration is provided by useminPrepare
+            dist: {}
+        },
+        cssmin: {
+            dist: {}
+        },
+        filerev: {
+            options: {
+                encoding: "utf8",
+                algorithm: "md5",
+                length: 20
+            },
+            release: {
+                files: [{
+                    src: [
+                        "dist/js/*.js",
+                        "dist/css/*.css"
+                    ]
+                }]
+            }
+        },
+        usemin: {
+            html: ["dist/contactus.html", "dist/aboutus.html", "dist/index.html"],
+            options: {
+                assestsDirs: ["dist", "dist/css", "dist/js"]
+            }
+        },
+        htmlmin: {
+            dist: {
+                options: {
+                    collapseWhitespace: true
+                },
+                files: {
+                    "dist/index.html": "dist/index.html",
+                    "dist/contactus.html": "dist/contactus.html",
+                    "dist/aboutus.html": "dist/aboutus.html"
+                }
+            }
         }
     });
 
@@ -86,6 +162,13 @@ module.exports = function (grunt) {
     grunt,registerTask("build", [
         "clean",
         "copy",
-        "imagemin"
-    ])
+        "imagemin",
+        'useminPrepare',
+        'concat',
+        'cssmin',
+        'uglify',
+        'filerev',
+        'usemin',
+        "htmlmin"
+    ]);
 };
